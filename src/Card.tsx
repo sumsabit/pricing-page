5e02c32fa8081e5de2d9ecff101be087c652e1ad
sumsabit <sumeyasabit1@example.com>
2026-09-07 12:44:57 +0300

    fixing Card.jsx

--- src/Card.tsx
+++ src/Card.tsx
@@ -25,10 +25,17 @@ function Card({name, price , description, features, bgColor, buttonColor}: CardP
       <ol>
         {features.map((feature)=>{
            return( <li key={feature} className="flex gap-2">
-             <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
-             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-              stroke-linecap="round" stroke-linejoin="round" 
-             class="lucide lucide-circle-check-big">
+             <span><svg xmlns="http://www.w3.org/2000/svg"
+              width="16" 
+              height="16" 
+              viewBox="0 0 24 24" 
+              fill="none"
+              stroke="currentColor" 
+              stroke-width="2"
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              className="lucide lucide-circle-check-big"
+              >
              <path d="M21.801 10A10 10 0 1 1 17 3.335"/><path d="m9 11 3 3L22 4"/>
              </svg></span>{feature}</li>);
           
